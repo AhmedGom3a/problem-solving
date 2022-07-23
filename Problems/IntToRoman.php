198f3e068dcd6841289c1b891aa98797205dab38
Ahmed Gomaa <ahmed.gomaa5858@gmail.com>
2022-07-23 20:11:14 +0200

    IntToRoman

---
+++ Problems/IntToRoman.php
@@ -0,0 +1,45 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return String
+     */
+
+    private $romanSymbols = [
+        1000 => "M",
+        900 => "CM",
+        500 => "D",
+        400 => "CD",
+        100 => "C",
+        90 => "XC",
+        50 => "L",
+        40 => "XL",
+        10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
+    ];
+
+    function intToRoman($num) {
+
+        $result = "";
+        foreach($this->romanSymbols as $value => $symbol){
+            if(($num % $value) == $num) continue;
+
+            $rem = $num % $value;
+            $diff = $num - $rem;
+
+            $count = $diff / $value;
+            for($i=0; $i<$count; $i++){
+                $result .= $symbol;
+            }
+
+            $num = $rem;
+        }
+
+        return $result;
+    }
+}
