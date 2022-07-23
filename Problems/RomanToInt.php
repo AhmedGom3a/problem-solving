a364b22b993046311edd16f41e2b2cd64746cc2b
Ahmed Gomaa <ahmed.gomaa5858@gmail.com>
2022-07-23 19:26:03 +0200

    Roman To Integer

---
+++ Problems/RomanToInt.php
@@ -0,0 +1,39 @@
+<?php
+
+class Solution {
+
+    private $romanSymbols = [
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    ];
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt($chars) {
+        $sum = 0;
+
+        $current = $this->romanSymbols[$chars[0]];
+
+        for($i = 0; $i<strlen($chars); $i++)
+        {
+            $next = $this->romanSymbols[$chars[$i + 1]];
+
+            if($next > $current){
+                $sum -= $current;
+                $current = $next;
+                continue;
+            }
+
+            $sum += $current;
+            $current = $next;
+        }
+
+        return $sum;
+    }
+}
